4c4d64da512770509e9f4aa1454ee7761f404506
Onyldas <onyldas@gmail.com>
2017-12-05 01:03:07 +0100

    Add config.properties and change Main

--- Project3/src/games/Main.java
+++ Project3/src/games/Main.java
@@ -1,5 +1,8 @@
 package games;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,30 +11,29 @@ public class Main {
 	
 	private static Logger logger = LogManager.getLogger();
 	
-	public static void main(String[] args) {
-		
+	public static void play(Game game) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Choose your game : \n1 - Plus or Minus\n2 - Mastermind\n");
 		int choice = sc.nextInt();
 		
 		if (choice == 1) {
 			logger.info("Plus or Minus game chosen.\n");
-			PoM game = new PoM();
 			
 			System.out.println("Choose your mode : \n1 - Challenger\n2 - Defender\n3 - Duel\n");
 			choice = sc.nextInt();
 			
 			if(choice == 1){
 				logger.info("Challenger mode chosen.\n");
-				game.challenger();
+				((PoM) game).challenger();
 			}
 			else if(choice == 2) {
 				logger.info("Defender mode chosen.\n");
-				game.ia();
+				((PoM) game).ia();
 			}
 			else if(choice == 3) {
 				logger.info("Duel mode chosen.\n");
 			}
+			
 			
 		}
 		
@@ -40,6 +42,31 @@ public class Main {
 		} 
 		
 	    sc.close();
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		try {
+			InputStream fis = new FileInputStream("config.properties");
+			Properties prop = new Properties();
+			prop.load(fis);
+			
+			//Get the properties and cast them to integers
+			int combination_size = Integer.parseInt(prop.getProperty("combination_size"));
+			int nb_try = Integer.parseInt(prop.getProperty("nb_try"));
+			
+			//Launch the game with properties
+			Game game = new PoM(combination_size, nb_try);
+			play(game);
+			
+			
+		} catch (Exception e) {
+			logger.error(e,e);
+		}
+		
+		
+		
 	}
 	
 }
